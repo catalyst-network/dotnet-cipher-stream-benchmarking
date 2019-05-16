c5a85dc82e9440b05108569eac8ffc82f5d583db
Sanjay Patel <sanjay.patel@atlascity.io>
2019-05-16 14:04:47 +0100

    Remove *10

--- src/Salsa20Benchmark.cs
+++ src/Salsa20Benchmark.cs
@@ -13,18 +13,14 @@ namespace benchmarks
         [BenchmarkCategory("PRNG")]
         public void GenerateRandom()
         {
-
-            for (int v = 0; v < 10; v++)
+            byte[] rng = new byte[Program.RNG_COUNT * Program.INT_SIZE];
+            using (SymmetricAlgorithm salsa20 = new Salsa20())
+            using (ICryptoTransform encrypt = salsa20.CreateEncryptor(Guid.NewGuid().ToByteArray(),
+                Guid.NewGuid().ToByteArray().Take(8).ToArray()))
+            using (MemoryStream streamInput = new MemoryStream(rng, false))
+            using (CryptoStream streamEncrypted = new CryptoStream(streamInput, encrypt, CryptoStreamMode.Read))
             {
-                byte[] rng = new byte[Program.RNG_COUNT * Program.INT_SIZE];
-                using (SymmetricAlgorithm salsa20 = new Salsa20())
-                using (ICryptoTransform encrypt = salsa20.CreateEncryptor(Guid.NewGuid().ToByteArray(),
-                    Guid.NewGuid().ToByteArray().Take(8).ToArray()))
-                using (MemoryStream streamInput = new MemoryStream(rng, false))
-                using (CryptoStream streamEncrypted = new CryptoStream(streamInput, encrypt, CryptoStreamMode.Read))
-                {
-                    streamEncrypted.ReadAsync(rng).GetAwaiter().GetResult();
-                }
+                streamEncrypted.ReadAsync(rng).GetAwaiter().GetResult();
             }
         }
     }
